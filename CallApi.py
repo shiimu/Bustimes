7a1112d6514031bd4a277c058154421ead864d6d
Siim Laineste <siim.laineste@outlook.com>
2020-12-09 12:34:13 +0200

    Update CallApi.py
    Formatting comments

--- CallApi.py
+++ CallApi.py
@@ -6,7 +6,7 @@ import time
 url = "https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql"
 # Calling from Api. Queryng only the wanted stuff.
 # HSL id 113 facing kontula, 114 for mellunmäki
-payload = {"query":"{\n  stop(id: \"HSL:\")1472113 {  name   stoptimesWithoutPatterns{ scheduledArrival   scheduledDeparture    serviceDay   headsign trip{route{ shortName}}}}}"}
+payload = {"query":"{\n  stop(id: \"HSL:1472113\") {  name   stoptimesWithoutPatterns{ scheduledArrival   scheduledDeparture    serviceDay   headsign trip{route{ shortName}}}}}"}
 headers= {"Content-Type" : "application/json"}
 response = requests.request("POST", url, headers=headers, data = json.dumps(payload))
 # Dumping the payload in json format
@@ -22,7 +22,7 @@ stopwrap = datawrap['stop']
 stoptimes_wrap = stopwrap['stoptimesWithoutPatterns']
 stop_name = stopwrap['name']
 
-# Getting the bussign from the dictionary
+# Getting the bus sign from the dictionary
 tripWrap = stoptimes_wrap[0]['trip']
 routeWrap = tripWrap['route']
 busRouteSign = routeWrap['shortName']
@@ -31,8 +31,8 @@ busHeadSign = stoptimes_wrap[0]['headsign']
 print(stop_name)
 print(busRouteSign)
 print(busHeadSign)
-#    print(stoptimes_wrap[1]['serviceDay'])
-#    print(stoptimes_wrap[2]['scheduledArrival'])
+# print(stoptimes_wrap[1]['serviceDay'])
+# print(stoptimes_wrap[2]['scheduledArrival'])
 
 # Getting the next incoming bus time in utc
 # Its holding the values inside. So doing[1] will give the next busses time
