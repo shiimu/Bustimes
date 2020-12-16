740ae00158665ce01627bf3936d89646a42d03a0
Siim Laineste <siim.laineste@outlook.com>
2020-12-16 19:04:33 +0200

    updated the site to display time and simple businfo
    passed values from the api to html

--- CallApi.py
+++ CallApi.py
@@ -7,7 +7,7 @@ import datetime
 url = "https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql"
 # Calling from Api. Queryng only the wanted stuff.
 # HSL id 113 facing kontula, 114 for mellunmäki
-payload = {"query":"{\n  stop(id: \"HSL:1472114\") {  name   stoptimesWithoutPatterns{ scheduledArrival   scheduledDeparture    serviceDay   headsign trip{route{ shortName}}}}}"}
+payload = {"query":"{\n  stop(id: \"HSL:1472113\") {  name   stoptimesWithoutPatterns{ scheduledArrival   scheduledDeparture    serviceDay   headsign trip{route{ shortName}}}}}"}
 headers= {"Content-Type" : "application/json"}
 response = requests.request("POST", url, headers=headers, data = json.dumps(payload))
 # Dumping the payload in json format
--- webApp.py
+++ webApp.py
@@ -1,10 +1,28 @@
 from flask import Flask, render_template
-
+import time
 app = Flask(__name__)
 
 @app.route("/")
-def bustimes():    
-    return render_template('main.html')
+def bustimes():
+    from CallApi import busRouteSign, normLeft, busHeadSign
+
+    busK = busRouteSign
+    busR = busHeadSign
+    busT =  normLeft
+    busK2 = busRouteSign
+    busR2 = busHeadSign
+    busT2 =  normLeft
+    busK3 = busRouteSign
+    busR3 = busHeadSign
+    busT3 =  normLeft
+    busK4 = busRouteSign
+    busR4 = busHeadSign
+    busT4 =  normLeft
+    timeNow = time.strftime('%H:%M', time.localtime(time.time()))    
+    return render_template('main.html', timen = timeNow, busK1 = busK, busR1 = busR, busT1 = busT,
+                                                     busK22 = busK2, busR22 = busR2, busT22 = busT2,
+                                                     busK33 = busK3, busR33 = busR3, busT33 = busT3,
+                                                     busK44 = busK4, busR44 = busR4, busT44 = busT4)
 
 if __name__ == "__main__":
-    app.run()
+    app.run( debug = True )
